eb8bd10b2db8031981e210f94b9f419ddb73a411
yoyoping <zhangping_ymd@outlook.com>
2019-03-27 18:08:12 +0800

    增加Gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin'); //Gzip
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
@@ -10,40 +11,53 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias
       // .set('@', resolve('src'))
-      .set("@assets", resolve("src/assets"))
-      .set("@components", resolve("src/components"))
-      .set("@utils", resolve("src/utils"))
-      .set("@views", resolve("src/views"));
+      .set('@assets', resolve('src/assets'))
+      .set('@components', resolve('src/components'))
+      .set('@utils', resolve('src/utils'))
+      .set('@views', resolve('src/views'));
   },
-  configureWebpack: {
-    optimization: {
-      minimizer: [
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            compress: {
-              warnings: false,
-              drop_console: true, //console
-              drop_debugger: false,
-              pure_funcs: ["console.log"] //移除console
-            }
-          }
-        })
-      ]
-    },
-    // 取消关于打包报文件大小警告
-    performance: {
-      hints: false
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionPlugin({ //文件开启Gzip
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$', ),
+            threshold: 10240,
+            minRatio: 0.8
+          })
+        ],
+        optimization: {
+          minimizer: [
+            new UglifyJsPlugin({ // 取消打印断点
+              uglifyOptions: {
+                compress: {
+                  warnings: false,
+                  drop_console: true, //console
+                  drop_debugger: false,
+                  pure_funcs: ['console.log'] //移除console
+                }
+              }
+            })
+          ]
+        },
+        // 取消关于打包报文件大小警告
+        performance: {
+          hints: false
+        }
+      }
     }
   },
   devServer: {
-    port: 80,
+    port: 8080,
     // 设置代理
     proxy: {
-      "": {
-        target: "http://120.78.128.49:3000", // 域名
+      '': {
+        target: 'http://120.78.128.49:3000', // 域名
         changOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRequiresRewrite: {
-          "^/api": "/"
+          '^/api': '/'
         }
       }
     }
